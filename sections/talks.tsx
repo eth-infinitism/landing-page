7f01a5dac8c8746fd9e2a2e41eb04cfc0a9665ad
Tom Teman <tomteman42@gmail.com>
2023-02-27 15:20:24 +0200

    style updates to "talks"

--- sections/talks.tsx
+++ sections/talks.tsx
@@ -12,8 +12,8 @@ const TalkItem = ({ title, subtitle, url }: ITalkItem) => {
   return domLoaded ? (
     <div className="w-80 overflow-hidden bg-white rounded-xl">
       <ReactPlayer url={url} width={320} height={208} />
-      <div className="text-left block px-5 py-3">
-        <div className="font-bold text-base truncate">{title}</div>
+      <div title={`${title}: ${subtitle}`} className="text-left block px-5 py-3">
+        <div className="font-bold text-base">{title}</div>
         <div className="text-xs truncate">{subtitle}</div>
       </div>
     </div>
@@ -22,9 +22,9 @@ const TalkItem = ({ title, subtitle, url }: ITalkItem) => {
 
 const Talks = () => {
   return (
-    <section className="w-full text-center relative">
-      <h1 className="title">“Talking about 4337”</h1>
-      <div className="w-full flex flex-row flex-wrap justify-evenly mt-20 z-0 gap-y-7">
+    <section className="w-full text-center justify-center relative">
+      <h1 className="title">“Talkin' about 4337”</h1>
+      <div className="w-full flex flex-row flex-wrap justify-evenly mt-20 z-0 gap-x-8 gap-y-4">
         {TalkItems.map((item, idx) => (
           <TalkItem {...item} key={idx} />
         ))}
