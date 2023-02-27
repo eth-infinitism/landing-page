273e3ec73f76ce773262f672ba708a26b7565bf8
Tom Teman <tomteman42@gmail.com>
2023-02-27 15:20:41 +0200

    update "why"

--- sections/why.tsx
+++ sections/why.tsx
@@ -7,7 +7,7 @@ const WhyItem = ({ title, description, image }: IWhyItem) => {
     <div className="flex flex-col items-center text-black text-center w-full">
       <Image src={image} alt="" width={145} height={145} className="rounded-full mt-3 " />
       <h1 className="font-bold text-base">{title}</h1>
-      <p className="text-xxs mt-2">{description}</p>
+      <p className="text-s mt-2">{description}</p>
     </div>
   );
 };
@@ -17,12 +17,21 @@ export default function Why() {
     <section className="block lg:flex flex-row gap-20 items-center text-center">
       <div className="lg:max-w-md">
         <h1 className="title mb-14 text-center">Why ERC-4337?</h1>
-        <p>
+        <p className="mb-4">
           Co-authored by Vitalik Buterin, Yoav Weiss, Kristof Gazso, Namra Patel, Dror Tirosh, Shahaf Nacson and Tjaden
           Hess, ERC-4337 introduces account abstraction without any modifications to the core protocol. It achieves so
           by replicating the functionality of the transactions mempool in a higher-level system. However, instead of
           transactions, users send UserOperation objects to Ethereum nodes, and they package a set of these objects into
           a single transaction that gets included in the Ethereum chain.
+        </p>
+        <p className="mb-4">
+          Through ERC-4337 Account Abstraction is becoming widespread! Join the numerous projects in the ecosystem 
+          that understand that if we want to onboard the next billion users, accounts can't always be forced to choose between
+          security and usabilty - must offer users both.
+        </p>
+        <p>
+          ERC-4337 takes usability even one step further by introducing paymasters. This mechanism enables users to either pay gas 
+          fees using ERC-20 tokens instead of ETH or to rely on a third party to sponsor their gas fees altogether, all in a decentralized fashion.
         </p>
       </div>
       <div className="w-full grid lg:grid-cols-2 lg:grid-rows-2 gap-y-6 gap-x-14">
