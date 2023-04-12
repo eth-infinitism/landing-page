8c87a36e34d4cb4f0f9a1379b50c9adad9b65f56
Tom Teman <tomteman42@gmail.com>
2023-04-13 00:13:41 +0300

    fix brand responsiveness on mobile

--- sections/brand.tsx
+++ sections/brand.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Brand() {
   return (
     <section className="flex flex-row justify-between items-center">
-        <div className="p-12 bg-white/[0.7] rounded-3xl">
+      <div className="p-12 bg-white/[0.7] rounded-3xl w-full md:w-auto">
         <div>
           <h1 className="font-bold text-2xl mb-12">
             ERC-4337: <br />
@@ -16,13 +16,12 @@ export default function Brand() {
             new separate mempool. Bundlers package up a set of these objects into a single transaction by making a call to a
             special contract, and that transaction then gets included in a block.
           </p>
-
-          <div className="flex flex-row mt-10 gap-4">
-            <a className="button w-full button-primary" href="#join">Join The Community</a>
-            <a className="button w-full h-full" href="/docs">Read The Docs</a>
-          </div>
         </div>
-    </div>
-      </section>
+        <div className="flex flex-col md:flex-row pt-10 gap-4 md:mt-0">
+          <a className="button w-full md:w-auto button-primary" href="#join">Join The Community</a>
+          <a className="button w-full md:w-auto" href="/docs">Read The Docs</a>
+        </div>
+      </div>
+    </section>
   );
 }
